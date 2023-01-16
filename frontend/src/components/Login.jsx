b50548728d85296b4e89e76db5e3bb0572655732
Daniil Pershin <pershin.daniil.e@gmail.com>
2023-01-16 13:23:54 +0300

    feat(step2): add validation

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,21 @@
-import { Card, Col, Container, Form, FormFloating, Image, Row} from "react-bootstrap";
-import loginImage from "../assets/login.jpeg"
+import {Card, Col, Container, Image, Row} from "react-bootstrap";
+import loginImage from "../assets/login.jpeg";
+import {Field, Form, Formik} from 'formik';
+import * as Yup from 'yup';
+
+
+const SignupSchema = Yup.object().shape({
+    userName: Yup.string()
+        .min(3, 'Too Short!')
+        .max(20, 'Too Long!')
+        .required('Required'),
+    password: Yup.string()
+        .min(8, 'Too Short!')
+        .max(64, 'Too Long!')
+        .required('Required'),
+});
+
+
 
 function Login () {
     return (
@@ -11,18 +27,42 @@ function Login () {
                             <Col className="col-12 d-flex align-items-center justify-content-center" md={6}>
                                 <Image src={loginImage} roundedCircle={true} alt="Войти" draggable="false" unselectable="on"/>
                             </Col>
-                            <Col as={Form} className="col-12 mt-3 mt-md-0 form" md={6} >
-                                <h1 className="text-center mb-4">Войти</h1>
-                                <FormFloating className="mb-3">
-                                    <input name="username" autoComplete="username" required={true} placeholder="Логин" id="username" className="form-control"/>
-                                    <label>Логин</label>
-                                </FormFloating>
-                                <FormFloating className="mb-4">
-                                    <input name="password" autoComplete="current-password" required={true} placeholder="Пароль" type="password" id="password" className="form-control"/>
-                                    <label>Пароль</label>
-                                </FormFloating>
-                                <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-                            </Col>
+                            <Formik
+                                initialValues={{
+                                    userName: '',
+                                    password: '',
+                                }}
+                                validationSchema={SignupSchema}
+                                onSubmit={values => {
+                                    // same shape as initial values
+                                    console.log(values);
+                                }}
+                            >
+                                {({ errors, touched }) => (
+                                    <Form className="col-12 mt-3 mt-md-0 form" md={6}>
+                                        <h1 className="text-center mb-4">Войти</h1>
+                                        <div className="form-floating mb-3">
+                                                <Field name="userName" id="userName" className={"form-control " + (errors.userName && touched.userName ? "is-invalid" : null)} placeholder="Login"/>
+                                            <label htmlFor="userName">Login</label>
+
+                                            {errors.userName && touched.userName ? (
+                                                <div className="text-danger mt-2">{errors.userName}</div>
+                                            ) : null}
+
+                                        </div>
+                                        <div className="form-floating mb-4">
+                                            <Field name="password" type="password" className={"form-control " + (errors.password && touched.password ? "is-invalid": null)} placeholder="password"/>
+                                            <label htmlFor="password">Password</label>
+
+                                            {errors.password && touched.password ? (
+                                                <div className="text-danger mt-2">{errors.password}</div>
+                                            ) : null}
+
+                                        </div>
+                                        <button type="submit" className="w-100 mb-3 btn btn-outline-primary">LogIn</button>
+                                    </Form>
+                                )}
+                            </Formik>
                         </Row>
                         <Card.Footer className="p-4">
                             <div className="text-center">
